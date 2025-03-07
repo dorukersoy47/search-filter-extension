e8c70a0f656c7ede1bbd428e88f37c02e3695a69
Yusuf Karaköse <zcabyka@ucl.ac.uk>
2025-03-07 23:06:32 +0300

    Language and File Type search added

--- popup.js
+++ popup.js
@@ -2,7 +2,6 @@ document.addEventListener('DOMContentLoaded', (event) => {
   const selectElement = document.getElementById("lastUpdate");
   const savedValue = localStorage.getItem('lastUpdate');
   
-  // Set the initial value to "anytime" if no value is saved
   if (savedValue) {
     selectElement.value = savedValue;
   } else {
@@ -20,8 +19,12 @@ document.getElementById('searchForm').addEventListener('submit', function(event)
   event.preventDefault();
   const searchInput = document.getElementById("searchInput").value;
   const lastUpdate = document.getElementById("lastUpdate").value;
+  const country = document.getElementById("country").value;
+  const fileType = document.getElementById("fileType").value;
   let query = 'https://www.google.com/search?as_q=' + encodeURIComponent(searchInput);
   if (lastUpdate) query += `&tbs=${lastUpdate}`;
+  if (country) query += `&cr=${country}`;
+  if (fileType) query += `&as_filetype=${fileType}`;
 
   chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
     chrome.tabs.update(tabs[0].id, { url: query });
